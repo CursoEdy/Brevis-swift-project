b55b26903c2e968a9d0fc4468e884e512f4ed20d
edy alves <30.edyalves@gmail.com>
2024-11-17 20:03:46 -0300

    HotkeySectionView

---
+++ Brevis/View/HotkeySectionView.swift
@@ -0,0 +1,18 @@
+//
+//  HotkeySectionView.swift
+//  Brevis
+//
+//  Created by ednardo alves on 17/11/24.
+//
+
+import SwiftUI
+
+struct HotkeySectionView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HotkeySectionView()
+}
