e59a24826451cc5504bb22568dda19c775f4f194
edy alves <30.edyalves@gmail.com>
2024-11-17 17:18:40 -0300

    implementando HotkeyCategoryModel

---
+++ Brevis/Model/HotkeyCategoryModel.swift
@@ -0,0 +1,16 @@
+//
+//  HotkeyCategoryModel.swift
+//  Brevis
+//
+//  Created by ednardo alves on 17/11/24.
+//
+
+import Foundation
+
+
+struct HotkeyCategoryModel: Identifiable {
+    let id = UUID()
+    
+    let name: String
+    let hotkeyModels: [HotKeyModel]
+}
