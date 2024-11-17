7d65692af3a19de1b6b83f0f5a8a7a7d410cbaae
edy alves <30.edyalves@gmail.com>
2024-11-17 13:15:16 -0300

    Initial Commit

---
+++ Brevis/BrevisApp.swift
@@ -0,0 +1,17 @@
+//
+//  BrevisApp.swift
+//  Brevis
+//
+//  Created by ednardo alves on 17/11/24.
+//
+
+import SwiftUI
+
+@main
+struct BrevisApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
